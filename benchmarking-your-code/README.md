df406c5277a465f8514f77a2fea320e9306d77c6
carlobiermann <carlo.biermann@protonmail.com>
2022-11-21 08:20:56 +0100

    patch: Adding files

---
+++ benchmarking-your-code/README.md
@@ -0,0 +1,8 @@
+# Benchmarking your C++ code
+
+
+This directory features examples from The Cherno's [C++ benchmarking video](https://www.youtube.com/watch?v=YG4jexlSAjc), as well as 
+Klaus Iglberger's [CppCON talks on move semantics](https://www.youtube.com/watch?v=St0MNEU5b0o)
+
+
+## How to build and run
---
+++ benchmarking-your-code/benchmarking-example/main.cpp
@@ -0,0 +1,85 @@
+/// All code derived from The Cherno's benchmarking video
+/// https://www.youtube.com/watch?v=YG4jexlSAjc
+
+#include <iostream>
+#include <chrono>
+#include <array>
+#include <memory>
+
+
+/// A scope base timer
+class Timer {
+public:
+    Timer() {
+        m_start_timepoint = std::chrono::high_resolution_clock::now();
+    }
+
+    ~Timer() {
+        stop();
+    }
+
+    void stop() {
+        auto end_timepoint = std::chrono::high_resolution_clock::now();
+
+        auto start = std::chrono::time_point_cast<std::chrono::microseconds>(m_start_timepoint).time_since_epoch().count();
+        auto end = std::chrono::time_point_cast<std::chrono::microseconds>(end_timepoint).time_since_epoch().count();
+
+        auto duration = end - start;
+        double ms = duration * 0.001;
+
+        std::cout << duration << "us (" << ms << "ms)\n";
+    }
+
+
+private:
+    std::chrono::time_point<std::chrono::high_resolution_clock> m_start_timepoint;
+};
+
+int main() {
+    int value = 0;
+    {
+        Timer timer;
+        for (size_t i = 0; i < 1000000; i++) {
+            value += 2;
+        }
+    }
+    std::cout << value << std::endl;
+
+    /// Profiling make_shared and make_unique in Release and Debug
+    /// It's important to profile in Release mode
+    struct Vector2 {
+        float x,y;
+    };
+
+    std::cout << "Make shared" << std::endl;
+    {
+        std::array<std::shared_ptr<Vector2>, 1000> shared_ptrs;
+        Timer timer;
+        for (size_t i = 0; i < shared_ptrs.size(); i++) {
+            shared_ptrs[i] = std::make_shared<Vector2>();
+        }
+    }
+
+    std::cout << "Make new" << std::endl;
+    {
+        std::array<std::shared_ptr<Vector2>, 1000> shared_ptrs;
+        Timer timer;
+        for (size_t i = 0; i < shared_ptrs.size(); i++) {
+            shared_ptrs[i] = std::shared_ptr<Vector2>(new Vector2());
+        }
+    }
+
+    std::cout << "Make unique" << std::endl;
+    {
+        std::array<std::unique_ptr<Vector2>, 1000> unique_ptrs;
+        Timer timer;
+        for (size_t i = 0; i < unique_ptrs.size(); i++) {
+            unique_ptrs[i] = std::make_unique<Vector2>();
+        }
+    }
+
+
+
+
+    return 0;
+}
